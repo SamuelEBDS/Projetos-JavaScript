4f52f8707fdf26a9452aca804dc7b973bde0b546
SamuelEBDS <samuel.ebds@gmail.com>
2023-04-06 13:06:10 -0300

    teste commit

---
+++ semaforo/script.js
@@ -0,0 +1,8 @@
+const img = document.getElementById('img');
+const buttons = document.getElementById('buttons');
+
+const trafficLght = () => { //
+
+}
+
+buttons.addEventListener('click', trafficLght);
